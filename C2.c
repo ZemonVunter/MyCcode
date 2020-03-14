21fff2b46d96dd9256d7fc361f5f4a7ce3db0e7f
ZemonVunter <1752767673@qq.com>
2020-03-14 13:22:31 +0800

    测试

---
+++ C2.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#define MAX 1000;
+int main(){ 
+/*const int num = 10;
+num = 20;
+printf("num=%d\n", num);
+*/
+	/*const int n = 10;
+	int arr[n] = { 0 };*/
+	/*int a = max;
+	printf("a=%d\n",a);
+	*/
+
+	/*char arr1[] = "abc";
+	char arr2[] = { 'a', 'b', 'c' };
+	printf("%s\n", arr1);
+	printf("%d\n", strlen(arr2));
+*/
+	printf("c:\test\32\test.c");
+	printf("%d\n",strlen("c:\test\32\test.c"));
+	return 0;
+}
